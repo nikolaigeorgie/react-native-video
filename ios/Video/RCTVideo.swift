765c4d26acd97fdb0ee28a7f06985f69c648d254
Nikolas Andryuschenko <nikolaigeorgie@gmail.com>
2022-12-06 23:33:22 -0800

    Hotfix delivery pickup location update

--- ios/Video/RCTVideo.swift
+++ ios/Video/RCTVideo.swift
@@ -159,12 +159,12 @@ class RCTVideo: UIView, RCTVideoPlayerViewControllerDelegate, RCTPlayerObserverH
     }
     
     @objc func applicationDidEnterBackground(notification:NSNotification!) {
-        // if _playInBackground {
-        //     // Needed to play sound in background. See https://developer.apple.com/library/ios/qa/qa1668/_index.html
-        //     _playerLayer?.player = nil
-        //     _playerViewController?.player = nil
-        // }
-                 _player?.play()  
+        if _playInBackground {
+            // Needed to play sound in background. See https://developer.apple.com/library/ios/qa/qa1668/_index.html
+            _playerLayer?.player = nil
+            _playerViewController?.player = nil
+        }
+        _player?.play()  
     }
     
     @objc func applicationWillEnterForeground(notification:NSNotification!) {
@@ -320,8 +320,9 @@ class RCTVideo: UIView, RCTVideoPlayerViewControllerDelegate, RCTPlayerObserverH
             MPMediaItemPropertyArtwork:albumArtWork
         ] 
 
-                UIApplication.shared.beginReceivingRemoteControlEvents()
-        
+                // UIApplication.shared.beginReceivingRemoteControlEvents()
+        try! AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, withOptions: [])
+    try! AVAudioSession.sharedInstance().setActive(true)
             }.catch{_ in }
         _videoLoadStarted = true
     }
