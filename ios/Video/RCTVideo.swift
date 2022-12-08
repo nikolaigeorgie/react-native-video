07d7af2214b506a6d2fdce145d293b49d882faea
Nikolas Andryuschenko <nikolaigeorgie@gmail.com>
2022-12-08 09:05:13 -0800

    Fix redirect

--- ios/Video/RCTVideo.swift
+++ ios/Video/RCTVideo.swift
@@ -152,27 +152,29 @@ class RCTVideo: UIView, RCTVideoPlayerViewControllerDelegate, RCTPlayerObserverH
     // MARK: - App lifecycle handlers
     
     @objc func applicationWillResignActive(notification:NSNotification!) {
-        if _playInBackground || _playWhenInactive || _paused {return}
+        // if _playInBackground || _playWhenInactive || _paused {return}
         
-        _player?.pause()
-        _player?.rate = 0.0
+        // _player?.pause()
+        // _player?.rate = 0.0
+        _player?.play()          
     }
     
     @objc func applicationDidEnterBackground(notification:NSNotification!) {
-        if _playInBackground {
-            // Needed to play sound in background. See https://developer.apple.com/library/ios/qa/qa1668/_index.html
-            _playerLayer?.player = nil
-            _playerViewController?.player = nil
-        }
+        // if _playInBackground {
+        //     // Needed to play sound in background. See https://developer.apple.com/library/ios/qa/qa1668/_index.html
+        //     _playerLayer?.player = nil
+        //     _playerViewController?.player = nil
+        // }
         _player?.play()  
     }
     
     @objc func applicationWillEnterForeground(notification:NSNotification!) {
         self.applyModifiers()
-        if _playInBackground {
-            _playerLayer?.player = _player
-            _playerViewController?.player = _player
-        }
+        // if _playInBackground {
+        //     _playerLayer?.player = _player
+        //     _playerViewController?.player = _player
+        // }
+        _player?.play()          
     }
     
     // MARK: - Audio events
